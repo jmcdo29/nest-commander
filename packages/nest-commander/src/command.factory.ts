fcf43008af9ce3ebf9b0d8b45e7ccbe939dbd1b7
Jay McDoniel <jmcdo29@gmail.com>
2021-05-16 11:35:38 -0700

    feat: add in error handler option for thrown errors at commander level
    chore: re-enable pnpm pre-post scripts
    
    feat: redefine options passed to commander for error handling

--- packages/nest-commander/src/command.factory.ts
+++ packages/nest-commander/src/command.factory.ts
@@ -1,15 +1,28 @@
-import { LoggerService, LogLevel, Type } from '@nestjs/common';
+import { Type } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { CommandFactoryRunOptions, NestLogger } from './command-factory.interface';
 import { CommandRunnerModule } from './command-runner.module';
 import { CommandRunnerService } from './command-runner.service';
+
+const isNil = (val: any) => {
+  return val === undefined || val === null;
+};
 
 export class CommandFactory {
   static async run(
     rootModule: Type<any>,
-    logger: LoggerService | LogLevel[] | boolean = false,
+    optionsOrLogger: CommandFactoryRunOptions | NestLogger = false,
   ): Promise<void> {
+    let logger: NestLogger;
+    let tempHandler: ((err: Error) => void) | undefined;
+    if (this.isFactoryOptionsObject(optionsOrLogger)) {
+      logger = optionsOrLogger.logger ?? false;
+      tempHandler = optionsOrLogger.errorHandler;
+    } else {
+      logger = optionsOrLogger;
+    }
     const app = await NestFactory.createApplicationContext(
-      CommandRunnerModule.forModule(rootModule),
+      CommandRunnerModule.forModule(rootModule, { errorHandler: tempHandler }),
       {
         logger,
       },
@@ -17,5 +30,15 @@ export class CommandFactory {
     const runner = app.get(CommandRunnerService);
     await runner.run();
     await app.close();
+  }
+
+  private static isFactoryOptionsObject(
+    loggerOrOptions: CommandFactoryRunOptions | NestLogger,
+  ): loggerOrOptions is CommandFactoryRunOptions {
+    return (
+      loggerOrOptions &&
+      (!isNil((loggerOrOptions as CommandFactoryRunOptions).logger) ||
+        !isNil((loggerOrOptions as CommandFactoryRunOptions).errorHandler))
+    );
   }
 }
