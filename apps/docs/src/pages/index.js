f001f283b1b168d0befc97c40cd5e5a8391a6cd1
Jay McDoniel <jmcdo29@gmail.com>
2021-10-11 15:32:34 -0700

    docs: make react function shorter

--- apps/docs/src/pages/index.js
+++ apps/docs/src/pages/index.js
@@ -46,6 +46,17 @@ function Feature({ imageUrl, title, description }) {
   );
 }
 
+const GetStarted = ({ getStartedStyle }) => {
+  return (
+    <Link
+      className={clsx('button button--outline button--secondary button--lg', getStartedStyle)}
+      to={useBaseUrl('docs/')}
+    >
+      Get Started
+    </Link>
+  );
+};
+
 const Header = ({ siteConfig }) => {
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
@@ -58,15 +69,7 @@ const Header = ({ siteConfig }) => {
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link
-            className={clsx(
-              'button button--outline button--secondary button--lg',
-              styles.getStarted,
-            )}
-            to={useBaseUrl('docs/')}
-          >
-            Get Started
-          </Link>
+          <GetStarted getStartedStyle={styles.getStarted} />
         </div>
       </div>
     </header>
