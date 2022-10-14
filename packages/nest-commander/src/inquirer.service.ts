01d6f949867af06595931aecdd3c50f4cdd1d08c
Jay McDoniel <jmcdo29@gmail.com>
2022-10-14 14:13:46 -0700

    chore: update inquirer prompt generic

--- packages/nest-commander/src/inquirer.service.ts
+++ packages/nest-commander/src/inquirer.service.ts
@@ -1,6 +1,6 @@
 import { DiscoveredMethodWithMeta, DiscoveryService } from '@golevelup/nestjs-discovery';
 import { Inject, Injectable } from '@nestjs/common';
-import { DistinctQuestion, Inquirer as InquirerType } from 'inquirer';
+import { Answers, DistinctQuestion, Inquirer as InquirerType } from 'inquirer';
 import {
   InquirerKeysWithPossibleFunctionTypes,
   QuestionMetadata,
@@ -38,7 +38,10 @@ export class InquirerService {
   };
 
   ask = this.prompt;
-  async prompt<T>(questionSetName: string, options: Partial<T> | undefined): Promise<T> {
+  async prompt<T extends Answers>(
+    questionSetName: string,
+    options: Partial<T> | undefined,
+  ): Promise<T> {
     const rawQuestions = await this.findQuestionSet(questionSetName);
     const questions = await this.mapMetaQuestionToQuestion(rawQuestions);
     const answers = await this.inquirer.prompt<T>(questions, options);
